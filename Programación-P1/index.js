d60f303c0388daf6d0b71fc1168e5eebe175b305
denishdz2023 <139892797+denishdz2023@users.noreply.github.com>
2023-08-11 12:07:00 -0600

    Add files via upload
    Practica #1

---
+++ Programación-P1/index.js
@@ -0,0 +1,29 @@
+// let edad = prompt ("Introduzca su edad")
+
+// if(edad >  18 && edad <= 100){
+//     alert("El usuario es mayor de edad")
+// }else if (edad <  18 && edad > 1){
+//     alert("El usuario es menor de edad")
+// }else if(edad == 18){
+//     alert("El usuario tiene 18")
+// }else if(edad > 100){
+//     alert("El usuario es centenario")
+// }else if(edad <= 0){
+//     alert("Edad invalidad")
+// }
+
+// let i = 1
+// let suma = 0
+// while( i <=100){
+//     suma += i;
+//     i++;
+
+// }
+// alert(suma)
+
+
+let num = prompt("Ingrese un numero:")
+
+for(i=1; i=10; i++){
+    alert(num + "por" + i + "es igual a:" + (num*i))
+}
